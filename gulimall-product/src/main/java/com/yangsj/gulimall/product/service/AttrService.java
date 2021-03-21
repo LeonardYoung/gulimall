1c745f367b997fb7816ab73ae088dcdeb3ff1190
LeonardYoung <shaojun47@qq.com>
2021-03-21 14:52:24 +0800

    P75完成

--- gulimall-product/src/main/java/com/yangsj/gulimall/product/service/AttrService.java
+++ gulimall-product/src/main/java/com/yangsj/gulimall/product/service/AttrService.java
@@ -3,6 +3,7 @@ package com.yangsj.gulimall.product.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.yangsj.common.utils.PageUtils;
 import com.yangsj.gulimall.product.entity.AttrEntity;
+import com.yangsj.gulimall.product.vo.AttrVo;
 
 import java.util.Map;
 
@@ -16,5 +17,7 @@ import java.util.Map;
 public interface AttrService extends IService<AttrEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    void saveAttr(AttrVo attr);
 }
 
